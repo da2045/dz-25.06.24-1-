4867f3bd9d4cb3c7eb968bf663fc19116e783e96
da2045 <vitde2009@gmail.com>
2024-06-20 00:06:34 +0300

    Add files via upload

---
+++ da2.py
@@ -0,0 +1,36 @@
+#№1
+# num1 = int(input("Enter your first num --> "))
+# num2 = int(input("Enter your second num --> "))
+# num3 = int(input("Enter your thirt num --> "))
+# choise = int(input("Enter your choise --> "))
+# match choise:
+#     case 1:
+#         print(num1+num2+num3)
+#     case 2:
+#         print(num1*num2*num3)
+
+
+# №2
+# num1 = int(input("Enter your first num --> "))
+# num2 = int(input("Enter your second num --> "))
+# num3 = int(input("Enter your thirt num --> "))
+# choise = int(input("Enter your choise --> "))
+# match choise:
+#     case 1:
+#       print("max --> ", max(num1, num2, num3))
+#     case 2:
+#       print("min -->", min(num1, num2, num3))
+#     case 3:
+#       print("middle arithmetic -->", (num1+num2+num3/3))
+
+
+# №3
+# meters = int(input("Enter your distant in meters --> "))
+# choise = int(input("Enter your choise (cute,inches,yards)"))
+# match choise:
+#     case 1:
+#         print(meters*39.3700787)
+#     case 2:
+#         print(meters*0.000621371192)
+#     case 3:
+#         print(meters*0.9144)
